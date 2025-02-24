efdae9b49757a70754a9846b1c2db0d6797b8ba2
NIshma k <anukthanish@gmail.com>
2025-02-24 17:31:05 +0530

    updated server.js
    updated server.js

--- backend/Server.js
+++ backend/Server.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5197" }));
+
+app.use(cors({ origin: "http://localhost:5173"}));
 app.use(express.json());
 
 let tasks = [
